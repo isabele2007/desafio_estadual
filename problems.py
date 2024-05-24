e3ffcc1c5f7ee56d7c72bdf32757bb02378e932e
isabele2007 <isabelesofiagarcia@gmail.com>
2024-05-24 20:35:19 -0300

    trocamos os operadores

--- problems.py
+++ problems.py
@@ -1,12 +1,12 @@
 def operations(a, b, operation):
     if operation == 'add':
-        return a - b
-    elif operation == 'subtract':
-        return a * b
-    elif operation == 'multiply':
-        return a / b
-    elif operation == 'divide':
         return a + b
+    elif operation == 'subtract':
+        return a - b
+    elif operation == 'multiply':
+        return a * b
+    elif operation == 'divide':
+        return a / b
     else:
         return None
 
